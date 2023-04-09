ff15a9a32ddee3d9e4fdc296e9750feebb262daf
PINKpig_N <2728158310@qq.com>
2023-04-09 19:39:15 +0800

    my first commit

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,36 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <nvboard.h>
+#include <Vtop.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+void nvboard_bind_all_pins(Vtop *top);
+
+static TOP_NAME dut;
+int main(int argc, char ** argv){
+
+
+
+  //insert pins
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+  //sim_main.cpp
+
+
+  while (1) {
+  int a = rand() & 1;
+  int b = rand() & 1;
+  dut.a = a;
+  dut.b = b;
+  dut.eval();
+  printf("a = %d, b = %d, f = %d\n", a, b, dut.f);
+
+
+  assert(dut.f == (a ^ b));
+  //here is the end
+  nvboard_update();
 }
+
+  nvboard_quit();
+
+}
+
